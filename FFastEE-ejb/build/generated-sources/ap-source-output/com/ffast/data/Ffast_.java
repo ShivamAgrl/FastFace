8d5429dcfc3bce011531d285617aedd529a70316
Shivam Agarwal <shivam_agarwal199@yahoo.com>
2019-05-14 20:37:00 +0530

    subsequent changes

--- FFastEE-ejb/build/generated-sources/ap-source-output/com/ffast/data/Ffast_.java
+++ FFastEE-ejb/build/generated-sources/ap-source-output/com/ffast/data/Ffast_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-15T19:34:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-12T19:22:15")
 @StaticMetamodel(Ffast.class)
 public class Ffast_ { 
 
